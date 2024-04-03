6586d837e8d87fe4246aca72f880e56563089403
cavis <ryancavis@gmail.com>
2024-04-03 10:03:46 -0600

    Setup S3Queues

--- tables/daily_uniques.sql
+++ tables/daily_uniques.sql
@@ -1,10 +1,34 @@
 CREATE TABLE daily_uniques (
-  `podcast_id` UInt32,
-  `day` Date,
-  `last_7_rolling` UInt64,
-  `last_28_rolling` UInt64,
-  `calendar_week` UInt64,
-  `calendar_month` UInt64
+  podcast_id UInt32,
+  day Date,
+  last_7_rolling UInt64,
+  last_28_rolling UInt64,
+  calendar_week UInt64,
+  calendar_month UInt64
 )
 ENGINE = ReplacingMergeTree
-ORDER BY (podcast_id, day)
+ORDER BY (podcast_id, day);
+
+-- final daily rollups
+CREATE TABLE daily_uniques_queue (
+  podcast_id UInt32,
+  day Date,
+  last_7_rolling UInt64,
+  last_28_rolling UInt64,
+  calendar_week UInt64,
+  calendar_month UInt64
+)
+ENGINE = S3Queue('gs://prx-rollups-prod/20**/daily_uniques_*.parquet')
+SETTINGS mode = 'ordered';
+
+-- intra-day increments
+CREATE TABLE daily_uniques_incr (
+  podcast_id UInt32,
+  day Date,
+  last_7_rolling UInt64,
+  last_28_rolling UInt64,
+  calendar_week UInt64,
+  calendar_month UInt64
+)
+ENGINE = S3Queue('gs://prx-rollups-prod/_incr/daily_uniques_*.parquet')
+SETTINGS mode = 'ordered';
--- tables/hourly_downloads.sql
+++ tables/hourly_downloads.sql
@@ -1,9 +1,31 @@
 CREATE TABLE hourly_downloads (
-  `podcast_id` UInt32,
-  `feed_slug` String,
-  `episode_id` String,
-  `hour` DateTime,
-  `count` UInt64
+  podcast_id UInt32,
+  feed_slug String,
+  episode_id String,
+  hour DateTime,
+  count UInt64
 )
 ENGINE = ReplacingMergeTree
-ORDER BY (podcast_id, feed_slug, episode_id, hour)
+ORDER BY (podcast_id, feed_slug, episode_id, hour);
+
+-- final daily rollups
+CREATE TABLE hourly_downloads_queue (
+  podcast_id UInt32,
+  feed_slug String,
+  episode_id String,
+  hour DateTime,
+  count UInt64
+)
+ENGINE = S3Queue('gs://prx-rollups-prod/20**/hourly_downloads_*.parquet')
+SETTINGS mode = 'ordered';
+
+-- intra-day increments
+CREATE TABLE hourly_downloads_incr (
+  podcast_id UInt32,
+  feed_slug String,
+  episode_id String,
+  hour DateTime,
+  count UInt64
+)
+ENGINE = S3Queue('gs://prx-rollups-prod/_incr/hourly_downloads_*.parquet')
+SETTINGS mode = 'ordered';
